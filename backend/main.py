1176bf22a24f6ab8a9b302376e2251e084afa597
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2021-12-19 14:53:55 +0200

    added and tested the main endpoints

--- backend/main.py
+++ backend/main.py
@@ -8,35 +8,38 @@ import workable_jobs as workableJ
 app = FastAPI()
 
 
-# @app.get("/")
-# def home():
-#     return {"message": "Welcome home son!"}
+@app.get("/")
+def home():
+    return {"message": "Welcome home son!"}
 
-# @app.get("/all-jobs")
-# def fetchAll(query: Query("frontend")):
-#     all_jobs = []
-#     jobs_count = 0
-#     wuzzuf_jobs = fetchWuzzuf(query)
-#     all_jobs.append(wuzzuf_jobs)
-#     jobs_count += wuzzuf_jobs['count']
-#     workable_jobs = fetchWorkable(query)
-#     all_jobs.append(workable_jobs)
-#     jobs_count += workable_jobs['count']
-#     return {"count":jobs_count,"results":all_jobs}
 
-# @app.get("/wuzzuf")
-# def fetchWuzzuf(query: Query("frontend")):
-#     wuzzuf_jobs = wuzzufJ.getJobs(query)
-#     return wuzzuf_jobs
+@app.get("/all-jobs")
+def fetchAll(query: str):
+    all_jobs = []
+    jobs_count = 0
+    wuzzuf_jobs = fetchWuzzuf(query)
+    all_jobs.append(wuzzuf_jobs)
+    jobs_count += wuzzuf_jobs["count"]
+    workable_jobs = fetchWorkable(query)
+    all_jobs.append(workable_jobs)
+    jobs_count += workable_jobs["count"]
+    return {"total-count": jobs_count, "results": all_jobs}
 
-# @app.get("/workable")
-# def fetchWorkable(query: Query("frontend")):
-#     workable_jobs = workableJ.getJobs(query)
-#     return workable_jobs
+
+@app.get("/wuzzuf")
+def fetchWuzzuf(query: str):
+    wuzzuf_jobs = wuzzufJ.getJobs(query)
+    return wuzzuf_jobs
+
+
+@app.get("/workable")
+def fetchWorkable(query: str):
+    workable_jobs = workableJ.getJobs(query)
+    return workable_jobs
 
 
 wuzzuf_jobs = wuzzufJ.getJobs("front end development")
 workable_jobs = workableJ.getJobs("front end development")
 
-print(wuzzuf_jobs)
-print(workable_jobs)
+# print(wuzzuf_jobs)
+# print(workable_jobs)
