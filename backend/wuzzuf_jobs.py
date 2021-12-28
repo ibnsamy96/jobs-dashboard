dbb0b39ab1b64831119e4cef43e160c81600581a
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2021-12-28 04:30:09 +0200

    finished wuzzuf job object as the needed model

--- backend/wuzzuf_jobs.py
+++ backend/wuzzuf_jobs.py
@@ -25,22 +25,122 @@ async def fetchPageSoup(page_num: int, query: str):
     return soup
 
 
+def createJobObject(**kwargs):
+
+    job = {
+        "title": kwargs.get("title"),
+        "description": kwargs.get("description"),
+        "href": kwargs.get("href"),
+        "location": {
+            "city": kwargs.get("location_city"),
+            "country": kwargs.get("location_country"),
+        },
+        "company": {
+            "name": kwargs.get("company_title"),
+            "logo": kwargs.get("company_image"),
+            "url": kwargs.get("company_website"),
+        },
+    }
+
+    return job
+
+
+def parseJobTitle(job_div):
+    return (
+        job_div.find("div", {"class": "css-laomuu"}).find("h2").find("a").text.strip()
+    )
+
+
+def parseJobDescription(job_div):
+    full_description = ""
+    headlines = (
+        job_div.find("div", {"class": "css-y4udm8"})
+        .find("div", {"class": "css-1lh32fc"})
+        .find("a")
+    )
+
+    for headline in headlines:
+        full_description = headline.text.strip() + " · "
+
+    general_info = (
+        job_div.find("div", {"class": "css-y4udm8"})
+        .find("div", {"class": "css-1lh32fc"})
+        .next_sibling.text.strip()
+    )
+
+    full_description += general_info
+    return full_description
+
+
+def parseJobHRef(job_div):
+    job_relative_href = (
+        job_div.find("div", {"class": "css-laomuu"}).find("h2").find("a").get("href")
+    )
+    job_abslute_href = f"https://wuzzuf.net{job_relative_href}".split("?")[0]
+    return job_abslute_href
+
+
+def parseLocationCityAndCountry(job_div):
+    location = (
+        job_div.find("div", {"class": "css-laomuu"})
+        .find("div", {"class": "css-d7j1kk"})
+        .find("span")
+        .text
+    )
+    location_country = location.split(",")[-1].strip()
+    location_city = location.split(",")[-2].strip()
+
+    return location_city, location_country
+
+
+def parseCompanyTitle(job_div):
+    return (
+        job_div.find("div", {"class": "css-laomuu"})
+        .find("div", {"class": "css-d7j1kk"})
+        .find("a")
+        .text[:-1]
+        .strip()
+    )
+
+
+def parseCompanyImage(job_div):
+    return job_div.find("a").find("img").get("src")
+
+
+def parseCompanyWebsite(job_div):
+    return (
+        job_div.find("div", {"class": "css-laomuu"})
+        .find("div", {"class": "css-d7j1kk"})
+        .find("a")
+        .get("href")
+    )
+
 
 async def fetchAndParsePageJobs(page_num: int, query: str):
     page_jobs = []
     soup = await fetchPageSoup(page_num, query)
-    job_elements = soup.find_all("img", {"class": "css-17095x3"})
-    h2_elements = soup.find_all("h2", {"class": "css-m604qf"})
-    for h2_element in h2_elements:
-        print(job_elements)
-        # imgElement = job_element.get("a").get("img")
-        # print(imgElement.get("src"))
-        aElement = h2_element.find("a")
-        exact_job_href = f'https://wuzzuf.net{aElement.get("href")}'.split("?")[0]
-        job_info = {
-            "title": aElement.string,
-            "href": exact_job_href,
-        }
+    job_divs = soup.find_all("div", {"class": "css-pkv5jc"})
+    for job_div in job_divs:
+        title = parseJobTitle(job_div)
+        description = parseJobDescription(job_div)
+        href = parseJobHRef(job_div)
+
+        location_city, location_country = parseLocationCityAndCountry(job_div)
+
+        company_title = parseCompanyTitle(job_div)
+        company_image = parseCompanyImage(job_div)
+        company_website = parseCompanyWebsite(job_div)
+
+        job_info = createJobObject(
+            title=title,
+            company_image=company_image,
+            href=href,
+            description=description,
+            company_website=company_website,
+            company_title=company_title,
+            location_city=location_city,
+            location_country=location_country,
+        )
         page_jobs.append(job_info)
 
     return page_jobs
