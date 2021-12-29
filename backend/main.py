04d65d2361a176ca5609c799054111818d51be33
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2021-12-30 00:56:30 +0200

    finishedhandeled main idea of telegram_login endpoint with code and password from cli

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,8 @@ import wuzzuf_jobs as wuzzufJ
 import workable_jobs as workableJ
 import telegram_groups_jobs as telegramGJ
 
+import time
+
 nest_asyncio.apply()
 app = FastAPI()
 
@@ -30,7 +32,11 @@ def fetchAll(query: str):
 
 @app.get("/wuzzuf")
 async def fetchWuzzuf(query: str):
+    print("wuzzuf_api")
+    start = time.time()
     wuzzuf_jobs = await wuzzufJ.getJobs(query)
+    end = time.time()
+    print(end - start)
     return wuzzuf_jobs
 
 
@@ -45,15 +51,17 @@ async def loginTelegram(
     phone: str,
     code: str or None = None,
     password: str or None = None,
+    phone_code_hash: str or None = None,
 ):
     phone = f"+{phone}"
-    user_session_string = "1BJWap1wBuxP5Bw8EEUoMxgJoYsJLeUKCsQtOWoq9EazeWjBJluEsM2r3itAB5NRnQE_4viYrIla_VYBvXPO2f41MkIC7Fv058IuR4cVlNCfQWL3dliVwVC3TPNtkupcl1obFzPk6qoydi65SphdDMfWwZ1Sm5wUtfX5_WvusrMua3TIOJ7EL92lCiMWre_oqeQyn4Q1-tGrRsZn6vonUP4AjNnq_6xFJtWkHpzSzr6JoH2DuLCUbh6HUta6gZJZ1USfpuk-BDRibxIfBH3pS91txDvH5PNiWKkt_-Bcbt7VLr5fyzF2SyFe0gtCLgdGTX9au4P-es6Dj2w3HW5zestoCBVTlzks="
+    # user_session_string = "1BJWap1wBuxP5Bw8EEUoMxgJoYsJLeUKCsQtOWoq9EazeWjBJluEsM2r3itAB5NRnQE_4viYrIla_VYBvXPO2f41MkIC7Fv058IuR4cVlNCfQWL3dliVwVC3TPNtkupcl1obFzPk6qoydi65SphdDMfWwZ1Sm5wUtfX5_WvusrMua3TIOJ7EL92lCiMWre_oqeQyn4Q1-tGrRsZn6vonUP4AjNnq_6xFJtWkHpzSzr6JoH2DuLCUbh6HUta6gZJZ1USfpuk-BDRibxIfBH3pS91txDvH5PNiWKkt_-Bcbt7VLr5fyzF2SyFe0gtCLgdGTX9au4P-es6Dj2w3HW5zestoCBVTlzks="
 
     user_string = telegramGJ.init(
         phone_number=phone,
         code=code,
-        user_session_string=user_session_string,
+        # user_session_string=user_session_string,
         password=password,
+        phone_code_hash=phone_code_hash,
     )
     print({"phone": phone})
     return user_string
--- backend/telegram_groups_jobs.py
+++ backend/telegram_groups_jobs.py
@@ -12,7 +12,7 @@ import asyncio
 # nest_asyncio.apply()
 
 
-from telethon import TelegramClient
+from telethon import TelegramClient, client
 from telethon.sessions import StringSession
 from telethon.tl.types import User
 from telethon.tl.types.auth import SentCode
@@ -28,35 +28,78 @@ api_id = os.getenv("api_id")
 api_hash = os.getenv("api_hash")
 phone_number = os.getenv("phone_number")
 
+states_messages = {
+    "auth_succeeded": "Great, Welcome again {user_name}!",
+    "auth_failed": "Oops, Telegram login failed, try again after a while.",
+    "code_requested": "We sent code to your telegram account, please write it down.",
+    "password_required": "Your account has 2FA and needs your password to login.",
+}
+
 
 async def auth(
     phone_number: str,
     user_session_string: str or None = None,
     code: str or None = None,
     password: str or None = None,
+    phone_code_hash: str or None = None,
 ):
-
-    user: User or SentCode
+    state: dict
+    user: User or SentCode or None = None
     print(user_session_string)
     client = TelegramClient(StringSession(user_session_string), api_id, api_hash)
     await client.connect()
 
     if await client.is_user_authorized():
         user = await client.get_me()
-        return user, client
 
-    await client.send_code_request(phone_number)
+        state = {"code": "auth_succeeded", "message": states_messages["auth_succeeded"]}
+        # return state, user, client
+        return state, user
+    print({"code_before_if": code})
+    if not code:
+        # await client.send_code_request(phone_number)
+        phone_code_hash = await client.send_code_request(phone_number)
+        state = {
+            "code": "code_requested",
+            "message": states_messages["code_requested"],
+            "phone_code_hash": phone_code_hash,
+        }
+        # return state, user
+
     try:
+        # user = await client.sign_in(
+        #     phone=phone_number, code=code or input("Enter code: ")
+        # )
+        print({"code": code, "phone_code_hash": phone_code_hash})
         user = await client.sign_in(
             phone=phone_number, code=code or input("Enter code: ")
         )
+        # user = await client.sign_in(
+        #     phone=phone_number, code=code ,phone_code_hash=phone_code_hash
+        # )
     except SessionPasswordNeededError:
         user = await client.sign_in(password=password or input("Enter password: "))
+        # print({"password": password})
+        # if not password:
+        # state = {
+        # "code": "password_required",
+        # "message": states_messages["password_required"],
+        # }
+        # return state, user, client
+        # return state, user
+
+        # user = await client.sign_in(password=password)
 
     user_session_string = client.session.save()
-    print("User session string: " + user_session_string)
+    # print("User session string: " + user_session_string)
 
-    return user, client
+    state = {
+        "code": "auth_succeeded",
+        "message": states_messages["auth_succeeded"],
+        "user_session_string": user_session_string,
+    }
+    # return state, user, client
+    return state, user
 
 
 async def main(user: User or SentCode, client: TelegramClient):
@@ -122,18 +165,21 @@ def init(
     phone_number: str,
     code: str or None = None,
     password: str or None = None,
+    phone_code_hash: str or None = None,
     user_session_string: str or None = None,
 ):
     print(user_session_string)
     # user, client = asyncio.get_event_loop().run_until_complete(auth(phone_number,code=code,password=password))
-    user, client = asyncio.get_event_loop().run_until_complete(
+    state, user = asyncio.get_event_loop().run_until_complete(
         auth(
             phone_number=phone_number,
             user_session_string=user_session_string,
             password=password,
             code=code,
+            phone_code_hash=phone_code_hash,
         )
     )
-    asyncio.get_event_loop().run_until_complete(main(user, client))
     # print(user)
-    return user.stringify()
+    # asyncio.get_event_loop().run_until_complete(main(user, client))
+    # print(user)
+    return {"state": state}
