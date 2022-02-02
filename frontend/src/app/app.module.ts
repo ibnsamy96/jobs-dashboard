1f8b994aac1824e4f8352021440f09cf18297523
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2022-02-02 15:50:04 +0200

    fixed file import

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,15 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { JobComponent } from './shared/components/job/job.component';
-import { JobRetrievalComponent } from './services/job-retrieval/job-retrieval.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    JobComponent,
-    JobRetrievalComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, JobComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
