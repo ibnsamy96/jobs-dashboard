6aa347977e3ce244ee6468c07681f4dfb0a247c0
Mahmoud Samy <mahmoudsamy50@outlook.com>
2023-08-27 20:15:14 +0300

    Updated the api url

--- frontend/src/app/services/job-retrieval.service.ts
+++ frontend/src/app/services/job-retrieval.service.ts
@@ -1,28 +1,28 @@
-import { Injectable } from '@angular/core';
-import { Observable, pipe } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { Observable, pipe } from "rxjs";
+import { map, tap } from "rxjs/operators";
 
-import { Job } from '../shared/types/job.interface';
-import { JobsResponse } from '../shared/types/jobs-response.interface';
-import { HttpService } from './http.service';
-import { Endpoint } from '../shared/types/general-types.interface';
+import { Job } from "../shared/types/job.interface";
+import { JobsResponse } from "../shared/types/jobs-response.interface";
+import { HttpService } from "./http.service";
+import { Endpoint } from "../shared/types/general-types.interface";
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: "root",
 })
 export class JobRetrievalService {
-  url = 'https://ky3irq.deta.dev';
+	url = "https://old-jobs-dashboard.onrender.com";
 
-  constructor(private httpService: HttpService) {}
+	constructor(private httpService: HttpService) {}
 
-  getJobs(endpoint: Endpoint, query: string): Observable<Job[]> {
-    return this.httpService.get(`${this.url}/${endpoint}?query=${query}`).pipe(
-      tap(console.log),
+	getJobs(endpoint: Endpoint, query: string): Observable<Job[]> {
+		return this.httpService.get(`${this.url}/${endpoint}?query=${query}`).pipe(
+			tap(console.log),
 
-      map((response) => {
-        const { results: jobsList } = response as JobsResponse;
-        return jobsList;
-      })
-    );
-  }
+			map((response) => {
+				const { results: jobsList } = response as JobsResponse;
+				return jobsList;
+			})
+		);
+	}
 }
